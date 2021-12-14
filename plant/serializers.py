1cd877e212c68ca5db64e37b60611841e23f14a9
Borja <borja.sueiro@udc.es>
2021-12-14 19:37:48 +0100

    refactor: change HyperlinkedModelSerializer by ModelSerializer

--- plant/serializers.py
+++ plant/serializers.py
@@ -2,27 +2,27 @@ from rest_framework import serializers
 
 from .models import Changes, Transfer, Temperature, ReceptionSilo, FinalSilo, Changes
 
-class TransferSerializer(serializers.HyperlinkedModelSerializer):
+class TransferSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transfer
         fields = ('code','date', "reception_silo", 'final_silo')
 
-class TemperatureSerializer(serializers.HyperlinkedModelSerializer):
+class TemperatureSerializer(serializers.ModelSerializer):
     class Meta:
         model = Temperature
         fields = ('value', 'date', "reception_silo")
 
-class ReceptionSiloSerializer(serializers.HyperlinkedModelSerializer):
+class ReceptionSiloSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReceptionSilo
         fields = '__all__'
 
-class FinalSiloSerializer(serializers.HyperlinkedModelSerializer):
+class FinalSiloSerializer(serializers.ModelSerializer):
     class Meta:
         model = FinalSilo
         fields = ('code', 'type')
 
-class ChangesSerializer(serializers.HyperlinkedModelSerializer):
+class ChangesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Changes
         fields = ('date', 'silo_src', 'silo_dst')
--- transport/serializers.py
+++ transport/serializers.py
@@ -1,17 +1,17 @@
 from rest_framework import serializers
 
 from .models import Transport, Transporter, MilkDelivery
-class TransportSerializer(serializers.HyperlinkedModelSerializer):
+class TransportSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transport
         fields = ('car_registration', 'tank_code', 'current_volumn', 'current', 'capacity', 'transporter')
 
-class TransporterSerializer(serializers.HyperlinkedModelSerializer):
+class TransporterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transporter
         fields = ('code', 'name','nif')
 
-class MilkDeliverySerializer(serializers.HyperlinkedModelSerializer):
+class MilkDeliverySerializer(serializers.ModelSerializer):
     class Meta:
         model = MilkDelivery
         fields = ('code', 'date', 'test', "volumn",'reception_silo','temperature','transporter')
